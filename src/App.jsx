0f9d6a80dd841416ff80a21c5504ce48260b014c
AB-Niloy <arnob080100@gmail.com>
2026-03-17 04:33:38 +0600

    Setup DaisyUI & Tailwind with React Application

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import "./App.css";
+import DaisiNav from "./Components/DaisiNav/DaisiNav";
+import NavBar from "./Components/NavBar/NavBar";
+
+function App() {
+  return (
+    <>
+
+    <header>
+      <NavBar></NavBar>
+      <DaisiNav></DaisiNav>
+    </header>
+
+    <main>
+
+    </main>
+
+    </>
+  );
+}
+
+export default App;
