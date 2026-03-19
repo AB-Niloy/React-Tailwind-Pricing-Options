f1431f1e507e6dc7486399bfd969be0b4811614f
AB-Niloy <arnob080100@gmail.com>
2026-03-19 17:09:49 +0600

    Added charts

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import "./App.css";
 import DaisiNav from "./Components/DaisiNav/DaisiNav";
 import NavBar from "./Components/NavBar/NavBar";
 import PricingOptions from "./Components/PricingOptions/PricingOptions";
+import ResultsChart from "./Components/ResultsChart/ResultsChart";
 
 const pricingPromise = fetch('PricingData.json')
 .then(res => res.json())
@@ -17,9 +18,15 @@ function App() {
     </header>
 
     <main>
-      <Suspense fallback={<span className="loading loading-spinner loading-lg"></span>}>
+      <Suspense fallback={
+        <div className="flex justify-center items-center min-h-screen">
+          <span className="loading loading-spinner loading-lg"></span>
+        </div>}>
         <PricingOptions pricingPromise={pricingPromise}></PricingOptions>
       </Suspense>
+
+      <ResultsChart></ResultsChart>
+
     </main>
 
     </>
---
+++ src/Components/ResultsChart/ResultsChart.jsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import { Line, LineChart, Tooltip, XAxis, YAxis } from 'recharts';
+
+const resultData = [
+  { "id": 1, "name": "Rahim", "phy": 85, "che": 80, "math": 90 },
+  { "id": 2, "name": "Karim", "phy": 78, "che": 75, "math": 82 },
+  { "id": 3, "name": "Ayesha", "phy": 92, "che": 89, "math": 95 },
+  { "id": 4, "name": "Nusrat", "phy": 88, "che": 84, "math": 91 },
+  { "id": 5, "name": "Tanvir", "phy": 74, "che": 70, "math": 76 },
+  { "id": 6, "name": "Sohan", "phy": 81, "che": 79, "math": 85 },
+  { "id": 7, "name": "Mim", "phy": 69, "che": 65, "math": 72 },
+  { "id": 8, "name": "Arif", "phy": 95, "che": 93, "math": 97 },
+  { "id": 9, "name": "Jannat", "phy": 87, "che": 83, "math": 89 },
+  { "id": 10, "name": "Rafi", "phy": 76, "che": 73, "math": 80 }
+]
+
+const ResultsChart = () => {
+    return (
+        <div>
+            <LineChart width={800} height={500} data={resultData}>
+                <XAxis dataKey={'name'}></XAxis>
+                <YAxis></YAxis>
+                <Line dataKey={'math'}></Line>
+                <Line dataKey={'phy'} stroke='red'></Line>
+                <Line dataKey={'che'} stroke='green'></Line>
+                <Tooltip></Tooltip>
+            </LineChart>
+        </div>
+    );
+};
+
+export default ResultsChart;
